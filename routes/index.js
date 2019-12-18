737386a42245086c0f3acb748ff09b5dc781aa4e
macMe <macme@macMes-MacBook-Pro.local>
2019-12-19 00:51:30 +0100

    edit in progress

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,13 @@ const Anime = require('../models/Anime');
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
 // home Page
-router.get('/home', ensureAuthenticated, (req, res,next) =>{
-    Anime.find({},(err, returnValue)=>{
-        if(err){
+router.get('/home', ensureAuthenticated, (req, res, next) => {
+    Anime.find({}, (err, returnValue) => {
+        if (err) {
             console.log(err);
-        }
-        else {
-            res.render('home',{
-                returnValue:returnValue,
+        } else {
+            res.render('home', {
+                returnValue: returnValue,
                 user: req.user
             })
         }
@@ -26,35 +25,56 @@ router.get('/home', ensureAuthenticated, (req, res,next) =>{
 });
 
 
-//Edit Handler
-router.post('/edit',(req,res)=>{
-    /* in progress
-    const id = req.body['idAnime'];
-    console.log(id);
-    console.log('delete Handler redirected to home');
-    Anime.deleteOne({_id:id},(err)=>{
-        if(err){
-            console.log(err);
-            return;
-        }else{
-            res.redirect('/home');
-        }
+//Edit home Handler
+router.post('/edithome', (req, res) => {
+    const _id = req.body['idAnime'];
+    const _animeImage = req.body['animeImage'];
+    const _title = req.body['title'];
+    const _category = req.body['category'];
+    const _description = req.body['description'];
+    console.log('editHome: ', _id, _animeImage, _title, _category, _description);
+    res.render('edit', {
+        e_id: _id,
+        e_animeImage: _animeImage,
+        e_title: _title,
+        e_category: _category,
+        e_description: _description
+    })
+});
 
-    });*/
+// home Page
+router.get('/edit', ensureAuthenticated, (req, res) => {
+     const bod = req.body;
+     /*const ee_animeImage = ;
+     const ee_title = req.body['title'];
+     const ee_category = req.body['category'];
+     const ee_description = req.body['description'];*/
+    console.log('edit==>: ',bod);
+     res.render('edit', {
+         bod:bod
+     })
 });
 
 
+/*
+Anime.updateOne({_id:anime._id},anime,(err)=>{
+    if(err){console.log(err); }
+    else{
+        res.redirect('/home');
+
+    }
+});
+*/
+
 
 //delete Handler
-router.post('/delete',(req,res)=>{
+router.post('/delete', (req, res) => {
     const id = req.body['idAnime'];
-            console.log(id);
-            console.log('delete Handler redirected to home');
-    Anime.deleteOne({_id:id},(err)=>{
-        if(err){
+
+    Anime.deleteOne({_id: id}, (err) => {
+        if (err) {
             console.log(err);
-            return;
-        }else{
+        } else {
             res.redirect('/home');
         }
 
@@ -68,9 +88,9 @@ router.get('/add', ensureAuthenticated, (req, res) => res.render('add'));
 // Add Handler
 router.post('/add',
     (req, res) => {
-        const {animeImage, title,category, description} = req.body;
+        const {animeImage, title, category, description} = req.body;
         let errors = [];
-        if (!title || !category ||!description) {
+        if (!title || !category || !description) {
             errors.push({msg: 'Please enter all fields'});
         }
         if (errors.length > 0) {
@@ -95,7 +115,7 @@ router.post('/add',
                         'success_msg',
                         'Anime ', title, ' was added successfully'
                     );
-                    console.log(newAnime);
+
                     res.redirect('/home');
                 })
                 .catch(err => console.log(err));
