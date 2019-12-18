d96bb584f3f27127be566b0aab60581334b22793
macMe <macme@macMes-MacBook-Pro.local>
2019-12-18 16:46:19 +0100

    working on home and add page

---
+++ models/Anime.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const AnimeSchema = new mongoose.Schema({
+    animeImage: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+
+const Anime = mongoose.model('Anime', AnimeSchema);
+
+module.exports = Anime;
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,43 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
+// Anime Modele
+const Anime = require('../models/Anime');
+
+
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
-// Dashboard
+// home Page
 router.get('/home', ensureAuthenticated, (req, res) =>
     res.render('home', {
         user: req.user
     })
 );
+
+// Add Page
+router.get('/add', ensureAuthenticated, (req, res) => res.render('add'));
+
+// Add Handler
+router.post('/add',
+    (req, res) => {
+        const {animeImage,title,description}=req.body;
+        const newAnime = new Anime({
+            animeImage,
+            title,
+            description
+        }).save().then(() => {
+            req.flash(
+                'success_msg',
+                'Anime ',title, ' was added successfully'
+            );
+            console.log(newAnime);
+            res.redirect('/home');
+        })
+            .catch(err => console.log(err));
+    });
+
+
+
 
 module.exports = router;
